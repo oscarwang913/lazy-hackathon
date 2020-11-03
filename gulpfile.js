3df2368e3d6d7c99f5d35b53ec4b00403c1f9c11
Oscar Wang <garnett284@gmail.com>
2020-11-03 13:52:32 +0800

    minify html, js, css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,28 @@
-const { src, dest, } = require('gulp')
-const sass = require('gulp-sass');
+const { src, dest, parallel } = require('gulp')
+const htmlmin = require('gulp-htmlmin');
+const cleanCSS = require('gulp-clean-css');
+const minify = require("gulp-babel-minify");
 
 function sassToCss() {
-  return src('./css/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(dest('./public'))
+  return src('./src/css/*.css')
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(dest('./public/css'))
 }
 
-exports.default = sassToCss
+function miniHTML() {
+  return src('./src/html/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest('public/'));
+}
+
+function miniJS() {
+  return src('./src/js/*.js')
+    .pipe(minify({
+      mangle: {
+        keepClassName: true
+      }
+    }))
+    .pipe(dest('public/js'));
+}
+
+exports.default = parallel(miniJS, miniHTML, sassToCss)
